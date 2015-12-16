9215643ce5aa7ecab4a33527cb57373fae7790b7
Vijay Yadav <Vijay.Yadav@talentica.com>
2015-12-16 11:35:38 -0800

    now webpack config working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  entry: './script2.js',
-    output: {
-      path: __dirname,
-      filename: 'main.js'
-    }
+  entry: {
+    main: ['./script1.js', './script2.js']
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js'
+  }
 };
